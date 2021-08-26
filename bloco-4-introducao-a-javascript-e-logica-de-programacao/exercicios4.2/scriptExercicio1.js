0a8e90570f32b34dc5c485c2ede2aac3485c63a2
guilhermerocha1 <guilhermerochasilva318@outlook.com>
2021-08-26 16:09:19 -0300

    Exercicios Course

---
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/exercicios4.2/scriptExercicio1.js
@@ -0,0 +1,9 @@
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+//let menuServices = menu[1];
+//let indexOfPortifolio = menu[menu.length - 4];
+menu.unshift("Contato");
+
+console.log(menu);
+//console.log(indexOfPortifolio);
+
+//console.log(menuServices);
---
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/exercicios4.2/scriptFor.js
@@ -0,0 +1,6 @@
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+groceryList.push("Carne");
+
+for (let index of groceryList){
+    console.log(index);
+}
