f7d38cae620acaeec9ff9f196b19ac31fe8494fa
guilhermerocha1 <guilhermerochasilva318@outlook.com>
2021-08-27 21:23:44 -0300

    Exercicios 4.3 Logica de programacao

---
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/exercicio4.3/scriptExercicio01.js
@@ -0,0 +1,10 @@
+let n = 5;
+
+if (n > 1){
+    for (let lines = 1; lines<=n; lines+=1){
+        
+        for (let index = 1; index<=lines; index+=1){
+            console.log("*");
+        }
+    }
+}
