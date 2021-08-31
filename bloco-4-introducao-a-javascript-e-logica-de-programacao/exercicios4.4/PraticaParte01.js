1c5abc784defb20ec67cdde78f972ecb60ea462d
guilhermerocha1 <guilhermerochasilva318@outlook.com>
2021-08-30 22:17:07 -0300

    Resolvido os exercicios

---
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/exercicios4.4/PraticaParte01.js
@@ -0,0 +1,13 @@
+let info = {
+    personagem: ['Margarida','Tio Patinhas'],
+    origem: ['Pato Donald', 'Christmas on Bear Mountain, Dells Four Color Comics #178 '],
+    nota: ['Namorada do personagem principal nos quadrinhos do Pato Donald O último MacPatinhas'],
+    recorrente: 'Sim'
+  };
+
+  //console.log("Bem Vinda, "+info.personagem);
+
+console.log(info.personagem[0]+" é "+info.personagem[1]);
+console.log(info.origem[0]+info.origem[1]);
+console.log(info.nota[0]);
+console.log("Ambos recorrentes "+info.recorrente+" Atenção para essa última linha");
