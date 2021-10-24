a7ebf0784b4a66951110ae4c141dc392a78c5b7a
guilhermerocha1 <guilhermerochasilva318@outlook.com>
2021-10-24 02:51:37 -0300

    Exercicos 1 de higher ordem functions

--- fundamentos/bloco-8-introducao-higher-order/exercicio.js
+++ fundamentos/bloco-8-introducao-higher-order/exercicio.js
@@ -1,17 +1,14 @@
-const objectInfo = () => {
-  return {
-    nomeCompleto: "Guilherme",
-    email: `${nomeCompleto}@trybe.com`
-  }
-}
-
 const newEmployees = () => {
   const employees = {
-    id1: '', // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-    id2: '', // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-    id3: '', // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    id1: 'Pedro Guerra', // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: 'Luiza Drumond', // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: 'Carla Paiva', // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
   }
   return employees;
 };
 
-console.log(``)
+const createEmail = (objectName) => {
+  return `${objectName().id1}@tryber.com`;
+}
+
+console.log(createEmail(newEmployees));
