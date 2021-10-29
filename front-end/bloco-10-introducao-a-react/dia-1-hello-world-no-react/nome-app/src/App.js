cde817414aec8aa56eabb28e15f5c20586630c18
guilhermerocha1 <guilhermerochasilva318@outlook.com>
2021-10-28 22:33:33 -0300

    Exercicio 1 terminado

--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/nome-app/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/nome-app/src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
+import Task from './Components/task';
 import './App.css';
 
-function App() {
-  return (
-    <h1>Hello Word</h1>
-  );
+class App extends React.Component {
+  render() {
+
+    return (
+      <>
+        <Task />
+        <h1>Hello Word</h1>
+      </>
+    );
+  }
 }
 
 export default App;
