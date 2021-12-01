cacc1d064177c8b9b19cdee085ba8816915f8492
guilhermerocha1 <guilhermerochasilva318@outlook.com>
2021-12-01 17:59:59 -0300

    Exercicio fixacao do course

--- fundamentos/bloco-9-javascript-e-testes-assincronos/dia-3-jest-testes-assincronos/script.test.js
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-3-jest-testes-assincronos/script.test.js
@@ -4,3 +4,9 @@ test('Verifica se o nome do herói é Wonder Woman', async () => {
   const hero = await getSuperHero('https://www.superheroapi.com/api.php/4192484924171229/720');
   expect(hero).toBe('Aquamen');
 });
+
+test('Testa a falha com a url alterada, lançando um erro', async () => {
+  const error = await getSuperHero('https://www.urlalterada.com');
+  expect(error.code).toBe('ENOTFOUND');
+  expect(error.message).toBe('request to https://www.urlalterada.com/ failed, reason: getaddrinfo ENOTFOUND www.urlalterada.com');
+});
