220e48a2493338644890ecf72ee8498ea3d3a1c4
guilhermerocha1 <guilhermerochasilva318@outlook.com>
2021-12-01 23:08:24 -0300

    Criando arquivos para exercicio

--- fundamentos/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/fetch.js
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/fetch.js
@@ -1,12 +1,15 @@
 const fetch = require('node-fetch');
 
-const fetchJoke = () => {
-  const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+const fetchJoke = async () => {
+	const url = 'https://api.chucknorris.io/jokes/random?category=dev';
 
-  fetch(url)
-  .then((response) => response.json())
-  .then((data) => console.log(data.value))
-  .catch((error) => console.log(`Alguma coisa está errado ${error}`))
+	try {
+		const response = await fetch(url)
+		const data = await response.json()
+	  console.log(data.value)
+	} catch (error) {
+    console.log(`Alguma coisa está errado ${error}`)
+  }
 };
 
 fetchJoke();
