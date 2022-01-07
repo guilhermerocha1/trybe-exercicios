cca1ecd4618930433d1bb0757376e30ede9b6e29
guilhermerocha1 <guilhermerochasilva318@outlook.com>
2022-01-06 23:56:53 -0300

    Implementando as primeiras labels e inputs

---
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/my-form-2.0/src/Form.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Fieldset from './Fieldset';
+
+class Form extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Formulário</h1>
+        <Fieldset />
+      </div>
+    )
+  }
+}
+
+export default Form;
